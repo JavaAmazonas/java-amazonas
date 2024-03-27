9db9ce26739a67dcf9e014af81dfcd7bb6729bfb
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-03-27 18:42:25 -0400

    blur no menu top e icon discord e wpp

--- src/app/page/header-landing-page/header-landing-page.component.ts
+++ src/app/page/header-landing-page/header-landing-page.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header-landing-page.component.html',
-  styleUrl: './header-landing-page.component.css'
+  styleUrl: './header-landing-page.component.css',
 })
 export class HeaderLandingPageComponent {
 
--- src/app/page/landing-page-inicio/landing-page-inicio.component.ts
+++ src/app/page/landing-page-inicio/landing-page-inicio.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderLandingPageComponent } from '../header-landing-page/header-landing-page.component';
 import { FooterComponent } from '../footer/footer.component';
 import { SliderEventComponent } from '../slider-event/slider-event.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page-inicio',
   standalone: true,
-  imports: [HeaderLandingPageComponent, FooterComponent,SliderEventComponent],
+  imports: [
+    HeaderLandingPageComponent,
+    FooterComponent,
+    SliderEventComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './landing-page-inicio.component.html',
-  styleUrl: './landing-page-inicio.component.css'
+  styleUrl: './landing-page-inicio.component.css',
 })
-export class LandingPageInicioComponent {
-
-}
+export class LandingPageInicioComponent {}
