0bc378a540d731ca1bf5ef9dd7d7988413ae459e
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-03-29 13:57:05 -0400

    ajuste

--- src/app/page/header-landing-page/header-landing-page.component.ts
+++ src/app/page/header-landing-page/header-landing-page.component.ts
@@ -1,19 +1,22 @@
 import { Component, ElementRef } from '@angular/core';
 import { ButtonToggleDarkLightComponent } from '../button-toggle-dark-light/button-toggle-dark-light.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header-landing-page',
   standalone: true,
-  imports: [ButtonToggleDarkLightComponent],
+  imports: [ButtonToggleDarkLightComponent,NgClass],
   templateUrl: './header-landing-page.component.html',
   styleUrl: './header-landing-page.component.css',
 })
 export class HeaderLandingPageComponent {
+   activeLink: any= 'inicio';
   inicio() {
     document.getElementById('inicio')?.scrollIntoView({ behavior: 'smooth' });
     if (window.innerWidth < 10) {
       this.removerHidden();
     }
+    this.activeLink = 'inicio';
   }
   porqueJava() {
     document
@@ -22,24 +25,30 @@ export class HeaderLandingPageComponent {
     if (window.innerWidth < 1024) {
       this.removerHidden();
     }
+    this.activeLink = 'porqueJava';
   }
   sobre() {
     document.getElementById('sobre')?.scrollIntoView({ behavior: 'smooth' });
     if (window.innerWidth < 1024) {
       this.removerHidden();
     }
+    this.activeLink = 'sobre'
+     // Ativa a classe dark:text-white
+
   }
   evento() {
     document.getElementById('evento')?.scrollIntoView({ behavior: 'smooth' });
     if (window.innerWidth < 1024) {
       this.removerHidden();
     }
+    this.activeLink = 'evento';
   }
   contato() {
     document.getElementById('contato')?.scrollIntoView({ behavior: 'smooth' });
     if (window.innerWidth < 1024) {
       this.removerHidden();
     }
+    this.activeLink = 'contato';
   }
 
   // Método para remover a classe 'hidden'
