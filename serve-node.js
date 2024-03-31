59c0e8fef0fe022af31bf9fe9fe43e8398f916de
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-03-30 22:59:29 -0400

    add youtube link no footer

--- serve-node.js
+++ serve-node.js
@@ -16,8 +16,8 @@ app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 //const server = https.createServer({
-//                    key: fs.readFileSync('/etc/letsencrypt/live/sisweb.trocados.com.br/privkey.pem'),
-//                  cert: fs.readFileSync('/etc/letsencrypt/live/sisweb.trocados.com.br/fullchain.pem')
+//                    key: fs.readFileSync('/etc/letsencrypt/live/javaAmazonas/privkey.pem'),
+//                  cert: fs.readFileSync('/etc/letsencrypt/live/javaAmazons/fullchain.pem')
 //                }, app).listen(port);
 
 server.listen(port, () => console.log("Funcionando"));
